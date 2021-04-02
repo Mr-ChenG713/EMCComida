6ea1beb52aef6edcbe23f7a61d1a0867793468c4
Mr.Cheng <mkt_aoo96@hotmail.com>
2021-04-02 05:23:21 +0100

    Ler dados Prefil

--- app/src/main/java/com/chengbo/emccomida/activity/HomeActivity.kt
+++ app/src/main/java/com/chengbo/emccomida/activity/HomeActivity.kt
@@ -6,17 +6,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 
 import com.chengbo.emccomida.R
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class HomeActivity : AppCompatActivity(){
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Iniciar Firebase
+        auth = Firebase.auth
+
+        var btn_logout = findViewById<Button>(R.id.btn_logout)
+        btn_logout.setOnClickListener {
+
+            logout()
+        }
     }
  
     fun abrirPrato (view: View){
@@ -41,6 +55,14 @@ class HomeActivity : AppCompatActivity(){
 
         val intent = Intent(this@HomeActivity, PrefilActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun logout(){
+
+        Firebase.auth.signOut()
+        val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
 }
--- app/src/main/java/com/chengbo/emccomida/activity/PrefilActivity.kt
+++ app/src/main/java/com/chengbo/emccomida/activity/PrefilActivity.kt
@@ -2,14 +2,70 @@ package com.chengbo.emccomida.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import com.chengbo.emccomida.R
+import com.chengbo.emccomida.model.Restaurantes
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class PrefilActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+    var databaseReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prefil)
 
         //ToolBar
         supportActionBar?.title = "Prefil"
+
+        //Iniciar Firebase
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("Restaurantes")
+
+
+        var btn_atualizar = findViewById<Button>(R.id.btn_update_profile)
+        btn_atualizar.setOnClickListener {
+
+        }
+
+        loadProfile()
+    }
+
+    private fun loadProfile() {
+
+        var text_nome = findViewById<TextView>(R.id.prefil_nome_show)
+        var text_email = findViewById<TextView>(R.id.prefil_email_show)
+        var text_contato = findViewById<TextView>(R.id.prefil_contato_show)
+        var text_morada = findViewById<TextView>(R.id.prefil_mora_show)
+        var text_cp = findViewById<TextView>(R.id.prefil_cp_show)
+        var text_cidade = findViewById<TextView>(R.id.prefil_cidade_show)
+
+
+        val user = auth.currentUser
+        val userreference = databaseReference?.child(user?.uid!!)
+
+        text_email.text =  user?.email
+
+        userreference?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                text_nome.text = snapshot.child("nome").value.toString()
+                text_contato.text = snapshot.child("contatotele").value.toString()
+                text_morada.text = snapshot.child("morada").value.toString()
+                text_cp.text = snapshot.child("codigopostal").value.toString()
+                text_cidade.text = snapshot.child("cidade").value.toString()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
